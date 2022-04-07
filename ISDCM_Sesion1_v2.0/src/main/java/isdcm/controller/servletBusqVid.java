c3f9ef37451daef3e08397c45254cdb9b7d4710c
bennyperez-cpu <55594332+bennyperez-cpu@users.noreply.github.com>
2022-04-07 04:15:06 +0200

    Other solution

--- ISDCM_Sesion1_v2.0/src/main/java/isdcm/controller/servletBusqVid.java
+++ ISDCM_Sesion1_v2.0/src/main/java/isdcm/controller/servletBusqVid.java
@@ -14,6 +14,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -247,66 +251,93 @@ public class servletBusqVid extends HttpServlet {
  
     }
         
-     /*       
-    public void show_visualization(HttpServletRequest request, HttpServletResponse response, String parameter, String enlace) throws IOException {
+           
+    public void show_Enlace_Data(HttpServletRequest request, HttpServletResponse response, String parameter, String enlace) throws IOException {
+
+        Client client = ClientBuilder.newClient();
+        Response response_1 = client.target("http://localhost:8080/ISDCM_Sesion2/resources/javaee8/getVideos")
+            .path("enlace").path(enlace).request(MediaType.APPLICATION_JSON).get();
+
+        ObjectMapper mapper = new ObjectMapper();
+        node = mapper.readTree(response.readEntity(String.class));
+        System.out.println(node.get("generalPractitioner").get(0).get("identifier").get("id"));
+
+
+
        
-        //String location = request.getParameter("location");
-        
 
-        Gson gson = new Gson();
- 
-        dataJson datajson = new dataJson(parameter,enlace);
-        //dataJson datajson = new dataJson("TITULO","La casa de Papel");
-        response.setContentType("application/json");
-        response.setCharacterEncoding("utf-8");
-        request.setCharacterEncoding("utf-8");
-        response.setStatus(200);
-        
-        String       postUrl       = "http://localhost:8080/ISDCM_Sesion2/ServletServer";// put in your url
 
-        HttpClient   httpClient    = HttpClientBuilder.create().build();
-        HttpPost     post          = new HttpPost(postUrl);
-        StringEntity postingString = new StringEntity(gson.toJson(datajson));//gson.tojson() converts your pojo to json
-        post.setEntity(postingString);
-        post.setHeader("Content-type", "application/json");
-        HttpResponse  response_2 = httpClient.execute(post);
-        //httpClient.execute(post);
-        log("Buscando por final" );
-        
-        HttpEntity entity = response_2.getEntity();
 
-        if (entity != null) {
 
-            InputStreamReader reader = new InputStreamReader(entity.getContent());
-            BufferedReader br = new BufferedReader(reader);
 
-            //BufferedReader br = request.getReader();
-            StringBuffer sb = new StringBuffer();
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-            log("Buscando " +  (sb.toString()));
+    
+    /*    
+        Client client = ClientBuilder.newClient();
+            Response response = client.target("http://localhost:8080/hapi_fhir_jpaserver_starter_war/fhir")
+                    .path("Patient").path(patientId).request(MediaType.APPLICATION_JSON).header("Authorization", "Bearer 39ff939jgg").get();
 
-            Gson gson_1 = new Gson();
-
-     //   video[] videos = gson_1.fromJson(new JsonReader(new FileReader(sb.toString())), video[].class);
-            java.lang.reflect.Type listType = new TypeToken<ArrayList<video>>(){}.getType();
-            List<video> list = new Gson().fromJson(sb.toString(), listType);
-        
-        log("El resultato 1 y 2" + list);
-        
-        
+        ObjectMapper mapper = new ObjectMapper();
+        node = mapper.readTree(response.readEntity(String.class));
+        System.out.println(node.get("generalPractitioner").get(0).get("identifier").get("id"));
 
         
         request.getSession().removeAttribute("play-video");
         response.sendRedirect("reproduccion.jsp");
         request.getSession().setAttribute("video", list);
+     */   
+
+        /*     
+    ClientConfig config = new DefaultClientConfig ();
+    Client client = Client.create (config); 
+    
+    WebResource service = client.resource(UriBuilder.fromUri("http://192.168.1.142:8080").build ()); // getting XML data 
+    String json = service.path("fhir").path("Patient/A").accept (MediaType.APPLICATION_JSON).get (String.class);
+ //           System.out.println(); // getting JSON data 
+ //           System.out.println(service.path("restPath").path("resourcePath").accept (MediaType.APPLICATION_XML).get (String.class));
+      
+        */
+
         
-        }
-    }  */  
+
+
+    }   
 
     /**
+     *  private WebTarget webTarget;
+    private Client client;
+    private static final String BASE_URI = "http://localhost:8080/ISDCM_Sesion2/resources";
+
+    
+    public ISDCM_Client() {
+        client = javax.ws.rs.client.ClientBuilder.newClient();
+        webTarget = client.target(BASE_URI).path("javaee8");
+    }
+
+    public String increReproducciones(String enlace) throws ClientErrorException {
+        Form form = new Form().param("enlace", enlace);        
+        return webTarget.path("postInfo").request(javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED)
+                .accept(javax.ws.rs.core.MediaType.TEXT_HTML).post(Entity.form(form), String.class);
+               
+    }
+
+    public String getReproducciones(String enlace) throws ClientErrorException {
+        WebTarget resource = webTarget;
+        if (enlace != null) {
+            resource = resource.queryParam("enlace", enlace);
+        }
+        resource = resource.path("getInfo");
+        return resource.request(javax.ws.rs.core.MediaType.TEXT_HTML).get(String.class);
+    }
+    
+    public <T> T lista_video(Class<T> responseType, String enlace) throws ClientErrorException {
+        WebTarget resource = webTarget;
+        if (enlace != null) {
+            resource = resource.queryParam("enlace", enlace);
+        }
+        resource = resource.path("getVideos");
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
+    }
+
      * Returns a short description of the servlet.
      *
      * @return a String containing servlet description
